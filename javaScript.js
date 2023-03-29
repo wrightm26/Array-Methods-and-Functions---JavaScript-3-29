6681703c055cda88862df55868dd537e21d30553
Mickel <mswright5981@gmail.com>
2023-03-29 01:27:23 -0400

    first commit

---
+++ javaScript.js
@@ -0,0 +1,49 @@
+// Exercise 1: Write a function called toCelcius that takes in a number representing
+// temperature in Fahrenheit and converts the number into Celcius.
+
+// Ex 1. toCelcius(32) // 0
+// Ex 2. toCelcius(212) // 100
+// Ex 3. toCelcius(50) // 10
+
+var toCelcius = (f) => ((f - 32) * 5) / 9;
+console.log(toCelcius(212))
+
+// Exercise 2: Create a class for a User that sets username and password property
+//     on creation. Add a method to change the user's password. The method should take in
+//     the old password and new password. If the old password is wrong do not reset the password.
+//     Create an instance of the new user and then change their password using the method.
+// */
+class User{
+    constructor(username, password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    changePassword(oldPassword, newPassword) {
+        console.log(this.username, this.password);
+        if (this.password === oldPassword){
+            this.password = newPassword;
+            console.log(this.password);
+        } else {console.log("Your old password is incorrect. We Cannot reset your password. Please try again!")};
+    };
+};
+
+let user1 = new User('brians', 'abc123');
+user1.changePassword('abc123', '123abc'); // Sets the user's password to 123abc
+
+
+// Exercise 3: Write a function that takes in an array of integers and returns
+//     an array of the negative integers squared using Array methods
+
+numbers = [2, 4, 6, 8, 10]
+
+posNum = numbers.filter((num) => num < 0);
+
+let squareNumber = posNum.map((num) => num = (Math.abs(num))**2);
+
+console.log(squareNumber)
+
+//console.log(squareNegatives([-10, -3, 4, -2, 8, 9, -3])) // [100, 9, 4, 9]
+//     Ex 2. squareNegatives([-5, -4, -3, -2, -1, 1, 2, 3, 4, 5]) // [25, 16, 9, 4, 1]
+//     Ex 3. squareNegatives([2, 4, 6, 8, 10]) // []
+//
